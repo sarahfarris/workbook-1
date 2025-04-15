3515a87a1b17d8410d95bec70ac5cbc3cb5c1fa8
sarahfarris <sarahfarris45@gmail.com>
2025-04-14 17:54:04 -0700

    New Exercises for 4-14

---
+++ first-mave-project/src/main/java/sandwichShop.java
@@ -0,0 +1,17 @@
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Scanner;
+
+public class sandwichShop {
+    public static int sizeOfSandwich(Scanner newScanner) {
+
+        System.out.println("What size sandwich do you want?");
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        String newScanner = new Scanner(System.in);
+
+
+    }
+}
